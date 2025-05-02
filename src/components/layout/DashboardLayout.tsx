7adf03c99eb7e8389b494f45e03dcb6784cac268
Marcos Antônio <108537878+M4rk1n02@users.noreply.github.com>
2025-05-02 13:02:20 -0300

    feat: uso do Outlet para layout

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,13 +1,15 @@
-import { Sidebar } from './sidebar';
+import { Sidebar } from './Sidebar';
 import { Header } from './Header';
+import { Outlet } from 'react-router';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout() {
   return (
     <div className="flex min-h-screen bg-[#f7f9fa]">
       <Sidebar />
       <div className="flex-1 flex flex-col md:ml-[255px]">
         <Header />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6">{children}</main>
+        <main></main>
+        <Outlet />
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App.tsx';
 import './index.css';
 import Dashboard from './pages/Dashboard.tsx';
+import DashboardLayout from './components/layout/DashboardLayout.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
