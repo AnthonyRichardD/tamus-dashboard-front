46c2b2cb65fbfc250070e0457038d99ecb5ab07b
Brenno Victor <brennovictor22@gmail.com>
2025-05-03 13:56:14 -0300

    feat: rota /recuperar-senha adicionada

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import RecoverPassword from './pages/RecoverPassword.tsx';
 import App from './App.tsx';
 import './index.css';
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/recuperar-senha" element={<RecoverPassword />} />
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
