85d62bece5fbee9e184e299d0cbec43308541aa1
Anthony <106710005+AnthonyRichardD@users.noreply.github.com>
2025-05-02 23:21:18 -0300

    fix: sidebar no mobile ocupando espaço do conteudo

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,38 +1,33 @@
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import { Button } from '@/components/ui/button';
-import { Menu } from 'lucide-react';
+import { Sheet, SheetContent, SheetTitle } from '@/components/ui/sheet';
 import { LogOutIcon } from 'lucide-react';
 
-export function Sidebar() {
+interface SidebarProps {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export function Sidebar({ isOpen, onOpenChange }: SidebarProps) {
   return (
     <>
       {/* Mobile */}
-      <Sheet>
-        <SheetTrigger asChild className="md:hidden">
-          <div className="flex h-16 items-center px-2.5 border-b bg-white">
-            <Button variant="ghost" size="icon" className="shrink-0">
-              <Menu className="h-5 w-5 " />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </div>
-        </SheetTrigger>
+      <Sheet open={isOpen} onOpenChange={onOpenChange}>
         <SheetContent side="left" className="w-[255px] px-0">
+          <SheetTitle className="sr-only">Menu de Navegação</SheetTitle>
           <DesktopSidebarContent />
         </SheetContent>
       </Sheet>
 
       {/* Desktop */}
-      <div className="hidden md:block fixed inset-y-0 left-0 w-[255px] bg-white border-r">
+      <div className="hidden md:block fixed inset-y-0 left-0 w-[255px] bg-white border-r z-50">
         <DesktopSidebarContent />
       </div>
     </>
   );
 }
-
 function DesktopSidebarContent() {
   return (
-    <div className=" justify-between flex h-full flex-col">
-      <div className="flex h-16 content-center items-center px-4 border-b gap-2">
+    <div className="justify-between flex h-full flex-col">
+      <div className="flex h-16 min-h-16 content-center items-center px-4 border-b gap-2">
         <div className="size-8 bg-teal-600 rounded-full flex items-center justify-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -49,7 +44,12 @@ function DesktopSidebarContent() {
         </div>
         <span>HealthScheduler</span>
       </div>
-      <div className="flex h-16 content-center items-center px-4 border-t gap-2 text-red-500">
+      <div className="w-full p-2 h-full">
+        <button className="w-full hover:bg-gray-200 flex items-center gap-2 rounded-md p-2">
+          <span>Dashboard</span>
+        </button>
+      </div>
+      <div className="flex h-16 min-h-16 content-center items-center px-4 border-t gap-2 text-red-500">
         <LogOutIcon />
         <span>Sair</span>
       </div>
