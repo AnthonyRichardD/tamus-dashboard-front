6adfe930a3ebb0726173b1fec063dfb62e3411aa
Brenno Victor <brennovictor22@gmail.com>
2025-05-15 21:27:29 -0300

    adição do método resetPassword na AdmniService

--- src/services/admin.services.ts
+++ src/services/admin.services.ts
@@ -21,12 +21,32 @@ interface ErrorResponse {
   message: string;
 }
 
+interface ResetPasswordRequest {
+  code: string;
+  new_password: string;
+}
+
+interface ResetPasswordResponse {
+  message: string;
+  is_error: boolean;
+}
+
 class AdminService {
   async login(loginData: UserLogin): Promise<UserLoginResponse> {
     try {
       const response = await api.post('/auth', loginData);
 
       localStorage.setItem('token', response.data.token);
+      return response.data;
+    } catch (error) {
+      return error as ErrorResponse;
+    }
+  }
+  async resetPassword(
+    resetData: ResetPasswordRequest
+  ): Promise<ResetPasswordResponse> {
+    try {
+      const response = await api.post('/admin/reset', resetData);
       return response.data;
     } catch (error) {
       return error as ErrorResponse;
