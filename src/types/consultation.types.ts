90d771efa4a581baf5d4113e315aaf213d96bfad
Anthony <106710005+AnthonyRichardD@users.noreply.github.com>
2025-08-03 15:10:47 -0300

    Update consultation.types.ts

--- src/types/consultation.types.ts
+++ src/types/consultation.types.ts
@@ -10,14 +10,8 @@ export interface ConsultationDetailsResponse {
 
 export interface Consultation {
   id: number;
-  patient_id: number;
-  professional_id: number;
-  consultation_type_id: number;
-  slot_id: number;
   status: ConsultationStatus;
   notes: string;
-  created_at: string;
-  updated_at: string;
   cancellation_reason: string;
   slot: Slot;
   patient: Patient;
@@ -32,44 +26,23 @@ export type ConsultationStatus =
   | 'no_show';
 
 export interface Slot {
-  id: number;
-  professional_id: number;
-  consultation_type_id: number;
   slot_date: string;
   start_time: string;
   end_time: string;
-  status: string;
 }
 
 export interface Patient {
-  id: number;
-  cpf: string;
   full_name: string;
-  email: string;
-  phone: string;
-  birth_date: string;
-  health_conditions?: string;
-  registration_date: string;
-  active: boolean;
-  reset_token_expira: string;
 }
 
 export interface Professional {
-  id: number;
   name: string;
   specialty: string;
-  registration_number: string;
-  active: boolean;
-  color_code: string;
-  createdAt: string;
-  updatedAt: string;
 }
 
 export interface ConsultationType {
-  id: number;
   name: string;
   duration_minutes: number;
-  active: boolean;
 }
 
 export interface Pagination {
